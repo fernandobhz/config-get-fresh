6989ca8ca0c76ca589cf8dee724d0c026ca40f6a
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-17 14:08:14 -0300

    fix: missing packages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 exports.get = (...args) => {
-  delete require.cache[join(__dirname, `node_modules`, `config`, `lib`, `config.js`)];
+  delete require.cache[path.join(__dirname, `node_modules`, `config`, `lib`, `config.js`)];
   const config = require("config");
   return config.get(...args);
 }
