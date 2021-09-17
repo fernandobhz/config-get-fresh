5d093c73c394dd0288583b89c83c6cdcc1a81e4d
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-17 15:47:31 -0300

    fix: node_modules should be from the main process

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 exports.get = (...args) => {
-  delete require.cache[path.join(__dirname, `node_modules`, `config`, `lib`, `config.js`)];
+  delete require.cache[path.join(process.cwd(), `node_modules`, `config`, `lib`, `config.js`)];
   const config = require("config");
   return config.get(...args);
 }
